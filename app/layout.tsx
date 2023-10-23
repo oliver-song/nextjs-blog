473271b2590419b7db082173b95b8bd74210f101
oli <zhidason@gmail.com>
2023-10-23 22:26:27 +0800

    fix bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
   const header = (
     <header>
       <div className='text-center bg-slate-800 p-8 my-6 rounded-md'>
-        <Image src="/logo.png" width={40} height={40} className="mx-auto" />
+        <Image src="/logo.png" alt="Logo" width={40} height={40} className="mx-auto" />
         <a href="/">
           <h1 className='text-2xl text-white font-bold mt-4'>Oli's Blog</h1>
         </a>
